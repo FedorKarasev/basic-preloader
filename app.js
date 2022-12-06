8e401642a504e08f0edf0b4b3a084ae2a748af2c
Fedor Karasev <fedorr.karasev@gmail.com>
2022-12-06 22:43:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', e => {
+    setTimeout(() => {
+        loader.classList.add('fadeOut');
+        setTimeout(() => loader.style.display = 'none', 1000);
+    }, 5000);
+});
